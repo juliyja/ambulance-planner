0dc2ef200ed2abf4473b2beb6dc6f5083bcb9ce8
juliyja <juljakubiak@gmail.com>
2020-03-03 21:32:04 +0000

    add threads

--- src/main/AmbulanceLauncher.py
+++ src/main/AmbulanceLauncher.py
@@ -1,15 +1,18 @@
-from main.RESTapi import app, close_connection
+from main.DatabaseUtil import print_time
+from main.RESTapi import app
 from main.XGboost import XGBoost
+from main.Planner import run_planner
+import _thread
 
 
 def main():
     print("Starting XGBoost...")
     XGBoost.train_data()
+    _thread.start_new_thread(print_time, ())
+    _thread.start_new_thread(run_planner, ())
     app.run(port='5002')
-    print("Starting Ambulance Planner")
+    print("Application closed")
 
-    print("Closing connection with the database...")
-    close_connection()
 
 if __name__ == "__main__":
     main()
--- src/main/RESTapi.py
+++ src/main/RESTapi.py
@@ -2,44 +2,36 @@ from flask import Flask, request
 from flask_restful import Api, abort
 import pymysql
 
+from main.DatabaseUtil import get_cursor, close_connection
+
 app = Flask(__name__)
 api = Api(app)
-db_connect = pymysql.connect(host='127.0.0.1',
-                                     database='AmbulanceDB',
-                                     user='root',
-                                     password='on@itsiriC_96')
-cursor = db_connect.cursor()
 
-print("MySQL connection is open")
 
 # after receiving a post request add it to database
 @app.route('/accident', methods=['POST'])
 def add_to_db():
     # print(request.json)
-    if not request.json or not 'Longitude' in request.json or not 'Latitude' in request.json:
+    if not request.json or 'Longitude' not in request.json or 'Latitude' not in request.json:
         abort(400)
 
     longitude = request.json['Longitude']
     latitude = request.json['Latitude']
     category = request.json['Category']
     req_vehicle_type = request.json['RequiredVehicle']
-    time = "NOW()"
+    time = request.json['Time']
     type = request.json['Type']
 
     mySql_insert_query = """INSERT INTO Accident (Longitude, Latitude, Category, RequiredVehicleType, Time, Type) 
                             VALUES 
                          (""" + str(longitude) + ", " + str(latitude) + ", " + category + ", " + \
-                         req_vehicle_type + ", " + time + ", " + str(type) + ")"""
+                         req_vehicle_type + ", " + "STR_TO_DATE('" + time + "','%Y-%m-%d %H:%i:%s'), " + str(type) + ")"""
+    print(mySql_insert_query)
 
+    cursor = get_cursor()
     cursor.execute(mySql_insert_query)
-    db_connect.commit()
-    # print(cursor.rowcount, "Accident record was successfully inserted into the database")
+    cursor.connection.commit()
+    close_connection(cursor)
 
-    return "Records added successfully into the database"
+    return "Accident records were successfully inserted into the database"
 
-
-def close_connection():
-    if db_connect.open:
-        cursor.close()
-        db_connect.close()
-        print("MySQL connection is closed")
