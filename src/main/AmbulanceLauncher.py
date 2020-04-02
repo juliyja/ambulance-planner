24349eac50b52bbda5dc578cfa5a4c10332dd42e
juliyja <juljakubiak@gmail.com>
2020-04-02 14:54:36 +0100

    add launching of the whole program

--- src/main/AmbulanceLauncher.py
+++ src/main/AmbulanceLauncher.py
@@ -1,5 +1,5 @@
 from main.AmbulanceDataUtil import fetch_ambulance_data, fetch_accident_data
-from main.NaturalLanguageProcessor import categorizeAccident
+from main.NaturalLanguageProcessor import categorize_accident
 from main.RESTapi import app
 from main.XGboost import XGBoost
 from main.Planner import run_planner
@@ -8,10 +8,10 @@ import _thread
 
 def main():
     print("Starting XGBoost...")
-    # XGBoost.train_data()
-    # _thread.start_new_thread(fetch_accident_data, ())
-    # _thread.start_new_thread(fetch_ambulance_data, ())
-    # _thread.start_new_thread(run_planner, ())
+    XGBoost.train_data()
+    _thread.start_new_thread(fetch_accident_data, ())
+    _thread.start_new_thread(fetch_ambulance_data, ())
+    _thread.start_new_thread(run_planner, ())
     app.run(port='5002')
     print("Application closed")
 
