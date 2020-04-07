c77b9635db1ac4053681d625b60013ba10fa6bd2
juliyja <juljakubiak@gmail.com>
2020-04-07 19:20:39 +0100

    fix fetching data and add a method for accessing hospital info

--- src/main/AmbulanceDataUtil.py
+++ src/main/AmbulanceDataUtil.py
@@ -1,11 +1,11 @@
 from main.DatabaseUtil import get_cursor
-from main.Planner import ambulances, accidents
+from main.Planner import ambulances, accidents, hospitals
 
 
 def fetch_ambulance_data():
     while True:
         cursor = get_cursor()
-        cursor.execute("SELECT * FROM Ambulance WHERE Assignment is 'Available'")
+        cursor.execute("SELECT * FROM Ambulance WHERE Assignment = 'Available'")
         rows = cursor.fetchall()
         for row in rows:
             ambulances.append(list(row))
@@ -14,8 +14,16 @@ def fetch_ambulance_data():
 def fetch_accident_data():
     while True:
         cursor = get_cursor()
-        cursor.execute("SELECT * FROM Accident WHERE Time <= NOW() AND Processed is NULL")
+        cursor.execute("SELECT * FROM Accident WHERE Time <= NOW() AND Processed IS NULL")
         rows = cursor.fetchall()
         for row in rows:
             accidents.append(list(row))
             cursor.execute("UPDATE Accident SET Processed = 1 WHERE AccidentId = " + str(row[0]))
+
+
+def detch_hospital_data():
+    cursor = get_cursor()
+    cursor.execute("SELECT * FROM Hospital")
+    rows = cursor.fetchall()
+    for row in rows:
+        hospitals.append(list(row))
